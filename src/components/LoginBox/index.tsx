c201cc3d88d730cadbe751a3e994a27b843217e8
Pedro Lucas <pedro19792001@gmail.com>
2021-10-23 13:50:01 -0300

    aplicando o context api no login box e mantendo o usuario conectado mesmo dps de att a pagina

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,43 +1,13 @@
-import { useEffect } from 'react';
+import { useContext } from 'react';
 import { VscGithubInverted } from 'react-icons/vsc';
-import { api } from '../../services/api';
+import { AuthContext } from '../../contexts/auth';
 import styles from './styles.module.scss';
 
-type AuthResponse = {
-    token: string;
-    user: {
-        id: string;
-        avatar_url: string;
-        name: string;
-        login: string;
-    }
-}
 
 export function LoginBox() {
-    const singInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=ba3f5fe6295e715afc6a`;
+    const { singInUrl, user } = useContext(AuthContext);
 
-    async function signIn(githubCode: string) {
-        const response = await api.post<AuthResponse>('authenticate', {
-            code: githubCode,
-        });
-
-        const { token, user } = response.data;
-        localStorage.setItem('@dowhile:token', token); //armazenando o token para que mesmo após fechar o navegador ele ainda tenha esse token salvo
-        console.log(user);
-    }
-
-
-    useEffect(() => {
-        const url = window.location.href;
-        const hasGithubCode = url.includes('?code=');
-        if (hasGithubCode) {
-            const [urlWithoutCode, githubCode] = url.split('?code=');
-
-            window.history.pushState({}, '', urlWithoutCode) //remove o codigo do github da url
-
-            signIn(githubCode);
-        }
-    }, [])
+    console.log(user);
 
     return (
         <div className={styles.loginBoxWrapper}>
